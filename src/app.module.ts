c4324740954dbe82ec1cc16d8705ae625d192247
Ben Botto <benjamin.botto@gmail.com>
2019-09-22 08:31:53 -0700

    Adds stub notice API.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { NoticeModule } from './notice/notice.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    NoticeModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
