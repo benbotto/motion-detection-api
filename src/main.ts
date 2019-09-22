182d46fbddf1748c9fdbdaa6f8372fc79578294b
Ben Botto <benjamin.botto@gmail.com>
2019-09-22 09:20:19 -0700

    Wires up the error handler.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { BsyErrorHandlerFilter } from 'formn-nestjs-utils';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Custom error handler.
+  app.useGlobalFilters(new BsyErrorHandlerFilter());
+
   await app.listen(3000);
 }
 bootstrap();
