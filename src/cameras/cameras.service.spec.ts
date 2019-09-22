cf0477b068fd5ebdc7fbbc07d82d6f63a59657da
Ben Botto <benjamin.botto@gmail.com>
2019-09-22 09:20:29 -0700

    Adds CamerasService#isUnique.

--- src/cameras/cameras.service.spec.ts
+++ src/cameras/cameras.service.spec.ts
@@ -16,4 +16,36 @@ describe('CamerasService()', () => {
     cameraDao = new CamerasDao(mockDC);
     cameraSvc = new CamerasService(mockDC, cameraDao);
   });
+
+  describe('.isUnique()', () => {
+    let selectSpy: jasmine.Spy;
+
+    beforeEach(() => {
+      selectSpy = mockDC.getExecuter().select as jasmine.Spy;
+    });
+
+    it('throws a DuplicateError if the ip or name is taken.', async () => {
+      selectSpy.and.returnValue(Promise.resolve([{'c.id': 42}]));
+
+      try {
+        await cameraSvc.isUnique('name', '192.168.3.4');
+        expect(true).toBe(false);
+      }
+      catch (err) {
+        expect(err.name).toBe('DuplicateError');
+      }
+    });
+
+    it('resolves if the camera is not a dupe.', async () => {
+      selectSpy.and.returnValue(Promise.resolve([]));
+
+      await cameraSvc.isUnique('fake name', '192.168.3.4', 42);
+
+      expect(selectSpy.calls.argsFor(0)[1]).toEqual({
+        name: 'fake name',
+        ip: '192.168.3.4',
+        excludeId: 42
+      });
+    });
+  });
 });
--- src/cameras/cameras.service.ts
+++ src/cameras/cameras.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
+import { DuplicateError } from 'bsy-error';
+
+import { ConditionBuilder } from 'formn';
 import { CRUDService, DataContextManager } from 'formn-nestjs-utils';
 
 import { Camera } from '../entity/camera.entity';
@@ -12,5 +15,26 @@ export class CamerasService extends CRUDService<Camera> {
     protected dao: CamerasDao) {
 
     super(dao);
+  }
+
+  /**
+   * Check if a camera is unique on name and ip (both independently)
+   * or raise a DuplicateError.
+   */
+  async isUnique(name: string, ip: string, excludeId?: number): Promise<void> {
+    const cb = new ConditionBuilder();
+    let cond = cb
+      .or(
+        cb.eq('c.name', ':name', name),
+        cb.eq('c.ip', ':ip', ip));
+
+    if (excludeId) {
+      cond = cb.and(cond, cb.neq('c.id', ':excludeId', excludeId));
+    }
+
+    const cams = await this.retrieve(cond);
+
+    if (cams.length)
+      throw new DuplicateError('Duplicate camera.', 'name-ip', cams[0].id);
   }
 }
