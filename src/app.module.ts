7fb23ec8a09cfa642c9b501993aba4ae4b24d8c3
Ben Botto <benjamin.botto@gmail.com>
2019-09-22 10:11:50 -0700

    Wires up formn.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 
+import { FormnModule } from 'formn-nestjs-utils';
+import { ConnectionsFileReader } from 'formn';
+
 import { CamerasModule } from './cameras/cameras.module';
 import { NoticeModule } from './notice/notice.module';
 
 @Module({
   imports: [
+    FormnModule
+      .forRoot(new ConnectionsFileReader()
+        .readConnectionOptions('./connections.json')[0]),
+
     CamerasModule,
     NoticeModule,
   ],
