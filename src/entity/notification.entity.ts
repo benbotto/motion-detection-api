44e3fa570944758ad4cbf74cdb088c6b6a79efd6
Ben Botto <benjamin.botto@gmail.com>
2019-09-22 19:07:07 -0700

    Entity for notifications.

---
+++ src/entity/notification.entity.ts
@@ -0,0 +1,18 @@
+import { Table, Column, ManyToOne } from 'formn';
+
+import { Camera } from './camera.entity';
+
+@Table({name: 'notifications'})
+export class Notification {
+  @Column({isNullable: false, sqlDataType: 'int'})
+  cameraId: number;
+
+  @Column({hasDefault: true, isNullable: false, sqlDataType: 'timestamp'})
+  createdOn: Date;
+
+  @Column({isPrimary: true, isGenerated: true, isNullable: false, sqlDataType: 'int'})
+  id: number;
+
+  @ManyToOne<Notification, Camera>(() => Camera, (l, r) => [l.cameraId, r.id])
+  camera: Camera;
+}
