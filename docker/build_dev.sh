e8a2bc56f156ada2ae46c94221b3c1fa7c16715b
Ben Botto <benjamin.botto@gmail.com>
2019-09-27 14:45:26 -0700

    Unused script.

--- docker/build_dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker rmi us.gcr.io/project-bgi/dispatch-api-dev:latest
-docker build \
-  --no-cache \
-  -t us.gcr.io/project-bgi/dispatch-api-dev:latest \
-  -f Dockerfile.dev .
-
-docker push us.gcr.io/project-bgi/dispatch-api-dev:latest
