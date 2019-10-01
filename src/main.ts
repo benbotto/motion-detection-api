b25e754f6be5a494f7e390dc1c068bcb5dbee522
Ben Botto <benjamin.botto@gmail.com>
2019-09-30 17:33:33 -0700

    Sets up a listener for RabbitMQ messages.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+
 import { AppModule } from './app.module';
 
 import { BsyErrorHandlerFilter } from 'formn-nestjs-utils';
@@ -8,6 +10,15 @@ async function bootstrap() {
 
   // Custom error handler.
   app.useGlobalFilters(new BsyErrorHandlerFilter());
+
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.MQ_CONNECTION_STRING],
+      queue: process.env.MQ_SAVE_QUEUE,
+      queueOptions: {durable: false},
+    }
+  });
 
   await app.startAllMicroservicesAsync();
   await app.listen(3000);
