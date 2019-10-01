1eaffe2d2a33d376ecfe2ac3fd8e244dc88ea54f
Ben Botto <benjamin.botto@gmail.com>
2019-09-30 17:27:02 -0700

    Dao for classifications.

---
+++ src/classifications/classifications.dao.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+import { Dao, DataContextManager } from 'formn-nestjs-utils';
+
+import { Classification } from '../entity/classification.entity';
+
+@Injectable()
+export class ClassificationsDao extends Dao<Classification> {
+  constructor(private dcMgr: DataContextManager) {
+    super(dcMgr, Classification, '');
+  }
+}
