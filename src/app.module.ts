91442e685ace6cd5869fc8b08e4aac86ff0c7a08
Ben Botto <benjamin.botto@gmail.com>
2019-09-30 17:35:00 -0700

    Exposes classifications APIs.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { FormnModule } from 'formn-nestjs-utils';
 
 import { CamerasModule } from './cameras/cameras.module';
+import { ClassificationsModule } from './classifications/classifications.module';
 import { MailModule } from './mail/mail.module';
 import { MotionRecordingsModule} from './motion-recordings/motion-recordings.module';
 import { NotificationsModule } from './notifications/notifications.module';
@@ -16,6 +17,7 @@ import { ConnectionsFileReader } from 'formn';
         .readConnectionOptions('./connections.json')[0]),
 
     CamerasModule,
+    ClassificationsModule,
     MailModule
       .forRoot(process.env.SENDGRID_API_KEY),
     MotionRecordingsModule,
