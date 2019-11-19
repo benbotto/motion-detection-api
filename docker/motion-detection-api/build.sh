94579d0f113fab87aef3f5e09e8af0ce944aee80
Ben Botto <benjamin.botto@gmail.com>
2019-11-18 18:03:12 -0800

    Increases wait time to 600s.

--- docker/motion-detection-api/build.sh
+++ docker/motion-detection-api/build.sh
@@ -8,20 +8,16 @@ fi
 
 TAG=$1
 
-# Cleanup.
-docker rmi avejidah/motion-detection-api:latest 2>/dev/null
-
 # Build, tagging as latest.
 docker build \
   --build-arg SSH_PRIVATE_KEY="$(<~/.ssh/id_rsa)" \
   --build-arg TAG="${TAG}" \
-  --no-cache \
-  -t avejidah/motion-detection-api:latest .
+  -t avejidah/motion-detection-api:latest-arm .
 
 # Push latest.
-docker push avejidah/motion-detection-api:latest
+docker push avejidah/motion-detection-api:latest-arm
 
 # Tag with a tag matching the code repository.
-docker tag avejidah/motion-detection-api:latest \
-  avejidah/motion-detection-api:${TAG}
-docker push avejidah/motion-detection-api:${TAG}
+docker tag avejidah/motion-detection-api:latest-arm \
+  avejidah/motion-detection-api:${TAG}-arm
+docker push avejidah/motion-detection-api:${TAG}-arm
